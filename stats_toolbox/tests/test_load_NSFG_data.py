600b94b09acb9390673753bacdb253300c2c8673
Chris Musselle <chris.j.musselle@gmail.com>
2015-04-25 22:11:10 +0100

    Added more tests for reading in NSFG data

--- stats_toolbox/tests/test_load_NSFG_data.py
+++ stats_toolbox/tests/test_load_NSFG_data.py
@@ -11,13 +11,23 @@ def test_load_fem_preg_2002():
 
     df = load_fem_preg_2002()
 
+    # Test the birth weights
     sorted_birth_wieght_frequencies = df.birthwgt_lb.value_counts().sort_index() 
 
     idx = [0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14., 15.]
     values = [8, 40, 53, 98, 229, 697, 2223, 3049, 1889, 623, 132, 26, 10, 3, 3, 1]
     target = pd.Series(data=values, index=idx)
 
-    # test index and values
     assert_true(all(sorted_birth_wieght_frequencies.index == target.index))
     assert_true(all(sorted_birth_wieght_frequencies.values == target.values))
+
+    # Test the birth order
+    bith_ord = df.birthord.value_counts().sort_index()
+    
+    idx = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    values = [4413, 2874, 1234, 421, 126, 50, 20, 7, 2, 1]
+    target = pd.Series(data=values, index=idx)
+
+    assert_true(all(bith_ord.index == target.index))
+    assert_true(all(bith_ord.values == target.values))
 
