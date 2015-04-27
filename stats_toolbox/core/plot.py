c93785a330c5464ec83c56fa3b20073e06444fad
Chris Musselle <chris.j.musselle@gmail.com>
2015-04-27 23:53:23 +0100

    Enhanced normal probabilty plot with sensible defaults + now used seaborn config file

--- stats_toolbox/core/plot.py
+++ stats_toolbox/core/plot.py
@@ -11,9 +11,10 @@ from .shared import _underride_dict, config_current_plot
 from .hist import Hist
 from .tools import fit_line
 from ..utils.data_generators import render_normal_probability
+from ..config import SEABORN_CONFIG
 
 
-def mulitplot(objects, plt_kwds=None, fig_kwds=None, **options):
+def mulitplot(objects, plt_kwds=None, fig_kwds=None, palette=None, **options):
     """Plot multiple stats objects on a single plot.
 
     Plots all objects on a single axes. Legend will reflect the 
@@ -24,6 +25,7 @@ def mulitplot(objects, plt_kwds=None, fig_kwds=None, **options):
       objects (list) : Either Hist, Pmf or Pdf objects.
       plt_kwds (dict) : Dictionary for keyword arguments passed to object.plot()
       fig_kwds (dict) : Dictionary for keyword arguments passed to matplotlib.pyplot.figure()
+      palette (list) : A list of colors to iterate over in all the line plots, if given.
       **options : Keyword arguments used for configuring axis, labels, titles.  
 
     Returns:
@@ -37,7 +39,11 @@ def mulitplot(objects, plt_kwds=None, fig_kwds=None, **options):
 
     fig = plt.figure(**fig_kwds)
     ax = fig.add_subplot(111)
-    palette = itertools.cycle(sb.color_palette())
+
+    if palette is None:
+        palette = itertools.cycle(sb.color_palette())
+    else: 
+        palette = itertools.cycle(palette) 
 
     for obj in objects:
         # Set some useful defaults if Hist is detected
@@ -59,6 +65,18 @@ def normal_probability_plot(sample, fit_color='0.8', **options):
     fit_color: color string for the fitted line
     options: passed along to Plot
     """
+    import seaborn as sb
+    sb.set_context(**SEABORN_CONFIG['context'])
+    sb.set_palette(SEABORN_CONFIG['pallet'])
+    sb.set_style(SEABORN_CONFIG['style'])
+
+    # Handle extra parameters parsed to config plot
+    plot_configs = dict()
+    for n in ['title', 'xlabel', 'ylabel', 'xscale', 'yscale',
+              'xticks', 'yticks', 'axis', 'xlim', 'ylim', 'legend']:
+        if n in options:  
+            plot_configs[n] = options.pop(n)
+
     xs, ys = render_normal_probability(sample)
 
     arr = np.asarray(sample)
@@ -75,6 +93,10 @@ def normal_probability_plot(sample, fit_color='0.8', **options):
     # Draw a second sample and plot alongside fitted model
     xs, ys = render_normal_probability(sample)
     ax.plot(xs, ys, **options)
+
+    plot_configs = _underride_dict(plot_configs, xlabel='Sample values', 
+        ylabel='Satandard Normal Sample', title='Normal Probability Plot')
+    config_current_plot(**plot_configs)
 
     return ax 
 
