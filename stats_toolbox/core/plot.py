83d3b8f277af33ef7248a528728bf2de43fbd4db
Chris Musselle <chris.j.musselle@gmail.com>
2015-05-02 13:06:00 +0100

    Added ability to plot multiple sequences in normal probability plot

--- stats_toolbox/core/plot.py
+++ stats_toolbox/core/plot.py
@@ -1,6 +1,7 @@
 """Thei module holds utility functions to generate a variety of statistical plots."""
 
 import itertools
+from collections import Iterable
 
 import numpy as np
 
@@ -9,6 +10,7 @@ import seaborn as sb
 
 from .shared import _underride_dict, config_current_plot
 from .hist import Hist
+from .cdf import Cdf
 from .tools import fit_line
 from ..utils.data_generators import render_normal_probability
 from ..config import SEABORN_CONFIG
@@ -31,7 +33,7 @@ def mulitplot(objects, plt_kwds=None, fig_kwds=None, palette=None, **options):
     Returns:
       fig : the matplotlib figure container for the plot. 
     """
-    # Initalise dictionaries for extra param handelig 
+    # Initalise dictionaries for extra param handelig
     if plt_kwds is None:
         plt_kwds = dict()
     if fig_kwds is None:
@@ -42,8 +44,8 @@ def mulitplot(objects, plt_kwds=None, fig_kwds=None, palette=None, **options):
 
     if palette is None:
         palette = itertools.cycle(sb.color_palette())
-    else: 
-        palette = itertools.cycle(palette) 
+    else:
+        palette = itertools.cycle(palette)
 
     for obj in objects:
         # Set some useful defaults if Hist is detected
@@ -58,12 +60,20 @@ def mulitplot(objects, plt_kwds=None, fig_kwds=None, palette=None, **options):
     return fig
 
 
-def normal_probability_plot(sample, fit_color='0.8', **options):
+def normal_probability_plot(samples, labels=None, fit_color='0.7', palette=None, **options):
     """Makes a normal probability plot with a fitted line.
 
-    sample: sequence of numbers
-    fit_color: color string for the fitted line
-    options: passed along to Plot
+    Args:
+        samples: sequence of numbers or a cdf object, or a list of sequences or cdf objects. 
+            If a list, all sequences / objects are plotted, but the fit line is only plotted 
+            for the first one in the list.
+        labels: sequence of labels for the samples parsed in.  
+        fit_color: color string for the fitted line
+        pallet: sequence of colurs to iterate over for the plots
+        options: passed along to Plot
+
+    Returns:
+      fig : the matplotlib figure container for the plot. 
     """
     import seaborn as sb
     sb.set_context(**SEABORN_CONFIG['context'])
@@ -74,30 +84,67 @@ def normal_probability_plot(sample, fit_color='0.8', **options):
     plot_configs = dict()
     for n in ['title', 'xlabel', 'ylabel', 'xscale', 'yscale',
               'xticks', 'yticks', 'axis', 'xlim', 'ylim', 'legend']:
-        if n in options:  
+        if n in options:
             plot_configs[n] = options.pop(n)
 
-    xs, ys = render_normal_probability(sample)
+    # Determine whether samples is a sequence or list of sequences.
+    if isinstance(samples, str):
+        raise TypeError(
+            'samples argument must be a sequence of numbers or a Cdf object, or a list thereof. Not a string.')
+    if not isinstance(samples, Iterable) and not isinstance(samples, Cdf):
+        raise TypeError(
+            'samples argument must be a sequence of numbers or a Cdf object, or a list thereof')
 
-    arr = np.asarray(sample)
-    mean = arr.mean()
-    std = arr.std()
+    iters_not_str = [(isinstance(s, Iterable) or isinstance(s, Cdf)) and not isinstance(s, str) for s in samples]
 
-    fit = fit_line(xs, mean, std)
+    if not all(iters_not_str):
+        # samples is a single sequence
+        samples = [samples]
 
+    # Set colour palette
+    if palette is None:
+        palette = itertools.cycle(sb.color_palette())
+    else: 
+        palette = itertools.cycle(palette) 
+
+    # initialise plot
     fig, ax = plt.subplots()
 
-    options = _underride_dict(options, linewidth=3, alpha=0.8)
-    ax.plot(*fit, color=fit_color, label='model', **options)
+    for idx, obj in enumerate(samples):
 
-    # Draw a second sample and plot alongside fitted model
-    xs, ys = render_normal_probability(sample)
-    ax.plot(xs, ys, **options)
+        if isinstance(obj, Cdf):
+            values = obj.xs
+        else:
+            values = obj
 
-    plot_configs = _underride_dict(plot_configs, xlabel='Sample values', 
-        ylabel='Satandard Normal Sample', title='Normal Probability Plot')
+        if idx == 0:
+            xs, ys = render_normal_probability(values)
+
+            arr = np.asarray(values)
+            mean = arr.mean()
+            std = arr.std()
+
+            fit = fit_line(xs, mean, std)
+            options = _underride_dict(options, linewidth=1.5)
+            ax.plot(*fit, color=fit_color, label='model', **options)
+
+        # Draw a sample and plot alongside fitted model
+        if labels is None:
+            if hasattr(obj, 'label'):
+                label = obj.label
+            else:
+                label = 'Sample %s' % idx
+
+        options = _underride_dict(options, linewidth=1.5, alpha=0.8)
+        xs, ys = render_normal_probability(values)
+        ax.plot(xs, ys, color=next(palette), label=label, **options)
+
+    # Add labels and title
+    plot_configs = _underride_dict(plot_configs, xlabel='Sample values',
+                                   ylabel='Standard Normal Sample', 
+                                   title='Normal Probability Plot',
+                                   legend=True)
+
     config_current_plot(**plot_configs)
 
-    return ax 
-
-
+    return fig
